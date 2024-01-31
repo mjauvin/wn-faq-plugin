c1d7dd8faf0a81712bce177e18682ea3a92bef0a
Marc Jauvin <marc.jauvin@gmail.com>
2024-01-31 10:24:52 -0500

    add improvements made for SSG

--- classes/ExtendFoundation.php
+++ classes/ExtendFoundation.php
@@ -1,11 +1,12 @@
 <?php namespace StudioAzura\FAQ\Classes;
 
 use App;
-use Backend;
 use BackendMenu;
 use Event;
+use StudioAzura\FAQ\Models\FAQ;
+use StudioAzura\Foundation\Models\Section;      
 use Yaml;
-use Lang;
+
 
 use StudioAzura\Foundation\Models\Section;
 
@@ -14,9 +15,22 @@ class ExtendFoundation
     public static function boot()
     {
         Section::extend(function () {
+            $this->data_attributes = array_merge($this->data_attributes, [
+                'faq',
+            ]);
+
             $this->section_types = array_merge($this->section_types, [
                 'faq',
             ]); 
         }, true);
+
+        Event::listen('backend.form.extendFieldsBefore', function ($formWidget) {
+            if ($formWidget->model instanceof Section) {
+                $faqTab = @Yaml::parseFile( plugins_path('studioazura/faq/config/faq_tab.yaml') );
+                $formWidget->tabs['fields'] = array_merge($formWidget->tabs['fields'], $faqTab);
+
+                $formWidget->tabs['fields']['image']['trigger']['condition'] .= '[faq]';
+            }
+        }); 
     }
 }
